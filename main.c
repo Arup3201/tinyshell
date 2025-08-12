2f06b6aacacbb53df1aaa6fd9380dc1928debdc5
Arup3201 <arupjana7365@gmail.com>
2025-08-12 21:18:10 +0530

    fix: program not terminated gracefully

--- main.c
+++ main.c
@@ -1,35 +1,32 @@
 // main.c: starting point of the tsh shell
 
 #include<stdio.h>
-#include<signal.h>
-#include<string.h>
 #include<stdlib.h>
+#include<signal.h>
 #include<readline/readline.h>
 #include<readline/history.h>
 
-volatile sig_atomic_t status = 0;
-void set_status(int sig) {
-	status = sig;
+void terminate_signal_handler(int signo) {
+	rl_clear_signals();
+	printf("\nExiting tinyshell...\n");
+	exit(EXIT_SUCCESS);
 }
 
 int main() {
 	char *cmd;
 
-	if(signal(SIGINT, set_status) == SIG_ERR) {
-		fputs("error handling signal", stderr);
+	// attach signal handler
+	if(signal(SIGINT, terminate_signal_handler) == SIG_ERR) {
+		fputs("error while attaching signal handler\n", stderr);
 		return 1;
 	}
 
 	while(1) {
-		if(status == SIGINT) {
-			free(cmd);
-			printf("exiting tinyshell\n");
-			return 0;
-		}
-		
 		cmd = readline("$> ");
 		
 		// Parse, expand and execute cmd
 		printf("Echo: %s\n", cmd);
+
+		free(cmd);
 	}
 }
